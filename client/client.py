c1b033e1477bf67fce0a27182f352a1f1baeee2d
Roel Zinkstok <rzinkstok@gmail.com>
2020-03-05 10:02:26 +0100

    Updates client for testing with FIFO

--- client/client.py
+++ client/client.py
@@ -5,6 +5,9 @@ import time
 from pylibftdi import Device, USB_PID_LIST, USB_VID_LIST
 from ctypes import *
 
+RANDOM = True
+INPUT = True
+
 STYX_VID = 0x2a19
 STYX_PID = 0x1007
 
@@ -30,26 +33,15 @@ def tobinary(i):
     print(f"{i:b}")
 
 
-def write_bytes(dev, b, y=False):
+def write_bytes(dev, b):
     print(len(b))
     m = slip(bytes(b))
     print(m)
-    #print(f"{i:08b}")
-    #if(len(m)>3):
-    #    print("-------------------------------------------------------------------------------")
     dev.write(m)
-    if y:
-        time.sleep(0.2)
+    if INPUT:
+        input()
     else:
-        input()
-
-
-def write_bytes_slow(dev, b):
-    m = slip(bytes(b))
-    for bb in m:
-        print(bb)
-        dev.write(bb)
-        input()
+        time.sleep(0.2)
 
 
 with Device() as dev:
@@ -61,34 +53,28 @@ with Device() as dev:
     dev.ftdi_fn.ftdi_setflowctrl(0)
     dev.ftdi_fn.ftdi_usb_purge_buffers()
 
-    if True:
+    i1 = 0
+    if RANDOM:
         while True:
-            i1 = random.randint(0, 255)
             i2 = random.randint(0, 255)
             i3 = random.randint(0, 255)
 
             print(i1, i2, i3)
-            write_bytes(dev, [i1, i2, i3], y=True)
+            write_bytes(dev, [i1, i2, i3])
+            i1 += 1
+            if i1 > 127:  # Make sure first MSB is 0 so the message is interpreted as 3 byte read command
+                i1 = 0
     else:
-        slow = False
         while True:
-            if slow:
-                write_bytes_slow(dev, [254, 253, 252])
-                write_bytes_slow(dev, [192, 219, 255])  # Escape END
-                write_bytes_slow(dev, [254, 13, 15])
-                write_bytes_slow(dev, [15, 219, 34])  # Escape ESC
-                write_bytes_slow(dev, [254, 1, 1])
-                continue
-            else:
-                write_bytes(dev, [254, 253, 252])
-                write_bytes(dev, [192, 219, 255])  # Escape END
-                write_bytes(dev, [254, 13, 15])
-                write_bytes(dev, [15, 219, 34])  # Escape ESC
-                write_bytes(dev, [254, 1, 1])
-                write_bytes(dev, [192, 1, 2, 3])
-                write_bytes(dev, [254, 2, 2])
-                write_bytes(dev, [2, 219])
-                write_bytes(dev, [2, 2, 219])
-                write_bytes(dev, [254])
-                write_bytes(dev, [254])
-                write_bytes(dev, [1, 2, 192])
+            write_bytes(dev, [254, 253, 252])
+            write_bytes(dev, [192, 219, 255])  # Escape END
+            write_bytes(dev, [254, 13, 15])
+            write_bytes(dev, [15, 219, 34])  # Escape ESC
+            write_bytes(dev, [254, 1, 1])
+            write_bytes(dev, [192, 1, 2, 3])
+            write_bytes(dev, [254, 2, 2])
+            write_bytes(dev, [2, 219])
+            write_bytes(dev, [2, 2, 219])
+            write_bytes(dev, [254])
+            write_bytes(dev, [254])
+            write_bytes(dev, [1, 2, 192])
